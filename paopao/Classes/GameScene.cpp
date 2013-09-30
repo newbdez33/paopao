3cd34f4c0d2a89cf4abf29c0c2a8faab1b2b6f07
Jacky <newbdez33@gmail.com>
2013-09-30 12:41:09 +0800

    清理项目，初始化

---
+++ paopao/Classes/GameScene.cpp
@@ -0,0 +1,43 @@
+#include "GameScene.h"
+#include "SimpleAudioEngine.h"
+
+using namespace cocos2d;
+using namespace CocosDenshion;
+
+CCScene* GameScene::scene()
+{
+    // 'scene' is an autorelease object
+    CCScene *scene = CCScene::create();
+    
+    // 'layer' is an autorelease object
+    GameScene *layer = GameScene::create();
+
+    // add layer as a child to scene
+    scene->addChild(layer);
+
+    // return the scene
+    return scene;
+}
+
+// on "init" you need to initialize your instance
+bool GameScene::init()
+{
+    //////////////////////////////
+    // 1. super init first
+    if ( !CCLayer::init() )
+    {
+        return false;
+    }
+    
+    CCSize size = CCDirector::sharedDirector()->getWinSize();
+    // add "HelloWorld" splash screen"
+    CCSprite* pSprite = CCSprite::create("icon120.png");
+
+    // position the sprite on the center of the screen
+    pSprite->setPosition( ccp(size.width/2, size.height/2) );
+
+    // add the sprite as a child to this layer
+    this->addChild(pSprite, 0);
+    
+    return true;
+}
--- paopao/Classes/GameScene.h
+++ paopao/Classes/GameScene.h
@@ -1,9 +1,9 @@
-#ifndef __HELLOWORLD_SCENE_H__
-#define __HELLOWORLD_SCENE_H__
+#ifndef __GameScene_SCENE_H__
+#define __GameScene_SCENE_H__
 
 #include "cocos2d.h"
 
-class HelloWorld : public cocos2d::CCLayer
+class GameScene : public cocos2d::CCLayer
 {
 public:
     // Method 'init' in cocos2d-x returns bool, instead of 'id' in cocos2d-iphone (an object pointer)
@@ -11,12 +11,9 @@ public:
 
     // there's no 'id' in cpp, so we recommend to return the class instance pointer
     static cocos2d::CCScene* scene();
-    
-    // a selector callback
-    void menuCloseCallback(CCObject* pSender);
 
     // preprocessor macro for "static create()" constructor ( node() deprecated )
-    CREATE_FUNC(HelloWorld);
+    CREATE_FUNC(GameScene);
 };
 
 #endif // __HELLOWORLD_SCENE_H__
